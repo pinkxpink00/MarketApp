a63d54887a206493db5730f1079dbbda803e7100
pinkxpink00 <kyperu07@gmail.com>
2023-06-22 00:05:34 +0300

    final 22.06

---
+++ MarketApp/MVVM/Model/EnterKeyHelper.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows;
+using System.Windows.Input;
+
+namespace CryptoCurrency.MVVM.AttachedProperties
+{
+	public class EnterKeyHelper
+	{
+		public static readonly DependencyProperty EnterKeyCommandProperty =
+			DependencyProperty.RegisterAttached("EnterKeyCommand", typeof(ICommand), typeof(EnterKeyHelper), new PropertyMetadata(null));
+
+		public static void SetEnterKeyCommand(UIElement element, ICommand value)
+		{
+			element.SetValue(EnterKeyCommandProperty, value);
+		}
+
+		public static ICommand GetEnterKeyCommand(UIElement element)
+		{
+			return (ICommand)element.GetValue(EnterKeyCommandProperty);
+		}
+	}
+}
