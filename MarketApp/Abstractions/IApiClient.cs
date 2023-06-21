511362758cd08c00e24bbb57545e91b848e79d0b
pinkxpink00 <kyperu07@gmail.com>
2023-06-21 06:37:22 +0300

    create IApiClient

---
+++ MarketApp/Abstractions/IApiClient.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CryptoCurrency.Abstractions
+{
+	public interface IApiClient
+	{
+		Task<T> GetAsync<T>(string url);
+	}
+}
